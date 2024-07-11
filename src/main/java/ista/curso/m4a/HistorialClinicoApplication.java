1efe78418a205d16731c532744791df2d6d6a482
Bfarez21 <bryanfareznieves@gmail.com>
2024-07-11 11:16:23 -0500

    Initial commit

---
+++ src/main/java/ista/curso/m4a/HistorialClinicoApplication.java
@@ -0,0 +1,13 @@
+package ista.curso.m4a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistorialClinicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistorialClinicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ista/curso/m4a/HistorialClinicoApplicationTests.java
@@ -0,0 +1,13 @@
+package ista.curso.m4a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistorialClinicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
